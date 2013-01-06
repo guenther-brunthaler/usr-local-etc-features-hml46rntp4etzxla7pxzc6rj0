73be060f3ebc47339bef34ec677da06772c1ca0a
Guenther Brunthaler <root@xeeepc.emgenxx69lwyn5ctlr4nl64ul.local>
2013-01-06 22:13:27 +0100

    Initial version
    This is actually a copy of some files from the local /etc directory,
    keeping the original bzr file ids.
    
    This branch is intended to be replicated to different boxes, and there
    to be merged into the local /etc branches.
    
    This is why this branch is called "etc_overlay" rather than just "etc".
    
    Initial branch nickname: sysconfig-overlay
    
    Initial check-out realm: emgenxx69lwyn5ctlr4nl64ul.local
    
    Initial check-out location: /etc
    
    Base-35 branch UUID: kn2rlg3hfgee18lhp4gbzzk51

---
+++ sysconfig_backups/refresh_backups.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+# Create files documenting the current system configuration, especially
+# partition layout and hardware configuration.
+#
+# The generated files as well as the configuration file for this script are
+# intended to be kept under version control. This allows to detect changes to
+# those configuration details later on.
+#
+# (c) 2011 - 2013 by Guenther Brunthaler.
+# This script is free software.
+# Distribution is permitted under the terms of the GPLv3.
+
+
+die() {
+	echo "ERROR: $*" >& 2
+	false; exit
+}
+
+
+run() {
+	"$@" && return
+	die "Command >>>$*<<< failed with return code ${?}!"
+}
+
+
+LC_ALL=C
+export LC_ALL
+for DEV in `cat "${0%.*}.config"`
+do
+	run test -b /dev/"$DEV"
+	run fdisk -lu /dev/"$DEV" | awk '
+		/[^ ]/ {print}
+		/dentifier:/ {exit}
+	' > disk-id_"$DEV"_info.txt
+	run sfdisk -d /dev/"$DEV" > sfdisk_"$DEV"_backup.txt
+done
+if LSHW=`which lshw 2> /dev/null`
+then
+	"$LSHW" > lshw.txt
+else
+	echo "lshw is not installed; skipping." >& 2
+fi
