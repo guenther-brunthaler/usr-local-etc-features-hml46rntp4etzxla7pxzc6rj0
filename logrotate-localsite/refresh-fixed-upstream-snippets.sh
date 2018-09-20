9a909226b88cc30264c0408fb18d0d17bbaf7da3
Guenther Brunthaler <root@xternal.emgenxx69lwyn5ctlr4nl64ul.local>
2018-09-20 15:27:30 +0200

    Support prioritized overrides for logrotation
    The site.d and realm.d subdirectories may contain override snippets with
    the same names as those deposited by packages in "/etc/logrotate.d".
    
    In this case, the snippets from the site.d (higher priority) or realm.d
    (lower priority) will be copied as the snippets to actually use, rather
    than trying to fix the package-provided versions of those snippets.

--- logrotate-localsite/refresh-fixed-upstream-snippets.sh
+++ logrotate-localsite/refresh-fixed-upstream-snippets.sh
@@ -2,15 +2,19 @@
 # This script can also be run from a cron job if standard error is redirected
 # to /dev/null. It only writes to /etc/ when actually necessary and avoids all
 # unnecessary write accesses (as long as $TMPDIR or /tmp is a tmpfs or ramfs).
+#
+# Version 2018.263
 
 basedir=/etc/logrotate-localsite
 defaults=$basedir/defaults
+overrides='site.d realm.d' # Highest priority first.
+
 
 set -e
 cleanup() {
-	local rc=$?
-	test -n "$T" && rm -- "$T"
-	test $rc = 0 || echo "$0 failed!" >& 2
+	rc=$?
+	test "$T" && rm -- "$T"
+	test $rc = 0 || echo "\"$0\" failed!" >& 2
 }
 T=
 trap cleanup 0
@@ -23,12 +27,16 @@ test -d "$indir"
 for o in "$outdir"/*
 do
 	test -e "$o" || continue # Required if directory is empty.
-	s=$indir/${o#"$outdir/"}
-	test -f "$s" && continue
+	ob=${o#"$outdir/"}
+	for s in "${indir#"$basedir/"}" $overrides
+	do
+		s=$basedir/$s/$ob
+		test -f "$s" && continue 2
+	done
 	echo "Removing orphaned '$o'..." >& 2
 	rm -- "$o"
 done
-# Most upstream snippets seem to indent by 4 spaces.
+# Most upstream snippets seem to be indented by 4 spaces.
 inc="    include $defaults"
 edscript='
 	# Matches the beginning of a local settings block.
@@ -47,12 +55,22 @@ edscript='
 		g # Restore current line to be printed at start of next cycle.
 	}
 '
-T=`mktemp "${TMPDIR:-/tmp}/${0##*/}.XXXXXXXXXX"`
+T=`mktemp -- "${TMPDIR:-/tmp}/${0##*/}.XXXXXXXXXX"`
 for s in "$indir"/*
 do
 	test -e "$s" || continue # Required if directory is empty.
-	o=$outdir/${s#"$indir/"}
-	sed "$edscript" < "$s" > "$T"
+	ob=${s#"$indir/"}
+	none=true
+	for o in $overrides
+	do
+		o=$basedir/$o/$ob
+		test ! -f "$o" && continue
+		cat < "$o" > "$T"
+		none=false
+		break
+	done
+	o=$outdir/$ob
+	$none && sed "$edscript" < "$s" > "$T"
 	# Avoid overwriting unless necessary to not disturb file mtime and
 	# ctime and thus not triggering unnecessary backups.
 	if cmp -s -- "$T" "$o"
@@ -60,7 +78,7 @@ do
 		:
 	else
 		echo "Generating '$o'..." >& 2
-		cat "$T" > "$o"
+		cat < "$T" > "$o"
 	fi
 done
 echo "Third-party logrotate snippet site-policy enforcement is up to date." \
