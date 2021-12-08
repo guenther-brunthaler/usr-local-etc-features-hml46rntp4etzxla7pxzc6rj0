ad861c475fbfd21281a9415243e2480653adc387
Guenther Brunthaler <root@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2021-12-08 02:51:55 +0100

    Support a defaults file
    This will save the user of modifying the script in most cases.

--- lsb-base-logging.sh
+++ lsb-base-logging.sh
@@ -4,19 +4,25 @@
 #
 # v2021.342
 
+# Define default values.
+
 # Create this empty directory in order for the scripts to mount a tmpfs
 # there and creating a log file within it.
 rclogmpt=/rclog
 rclogfile=log
+rclogmax=3g
+
+# Source defaults file (if any), allowing the user to override above settings.
+test -f /etc/defaults/rclog && . /etc/defaults/rclog
 
 test ! -d "$rclogmpt" && return # Do nothing if mount point dir does not exist.
 test -e "$rclogmpt"/no && return # Create tag file "no" there to stop logging.
 if test ! -e "$rclogmpt/$rclogfile"
 then
-	mount -t tmpfs -o size=3g "$rclogfile" "$rclogmpt"
+	mount -t tmpfs -o size=$rclogmax "$rclogfile" "$rclogmpt"
 fi
 exec >> "$rclogmpt/$rclogfile" 2>& 1
-unset rclogmpt rclogfile
+unset rclogmpt rclogfile rclogmax
 echo ------------
 date
 set -xv
